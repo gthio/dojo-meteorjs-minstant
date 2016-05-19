dfa6d65dbc8d240d3a1e38a6a2472d9bb6f63e5d
gthio <gunawanthio@outlook.com>
2016-05-19 23:34:16 +0800

    Meteor methods - for create and update chat, event handler and routing to use the 2 methods

--- minstant.js
+++ minstant.js
@@ -30,7 +30,11 @@ if (Meteor.isClient) {
     
     var chat = Chats.findOne(filter);
     if (!chat){// no chat matching the filter - need to insert a new one
-      chatId = Chats.insert({user1Id:Meteor.userId(), user2Id:otherUserId});
+      
+      var user1Id = Meteor.userId();
+      var user2Id = otherUserId;
+      
+      chatId = Meteor.call("createChat", user1Id, user2Id);
     }
     else {// there is a chat going already - use that. 
       chatId = chat._id;
@@ -91,36 +95,15 @@ if (Meteor.isClient) {
 
   Template.chat_page.events({
 
-    // this event fires when the user sends a message on the chat page
     'submit .js-send-chat':function(event){
 
       // stop the form from triggering a page reload
       event.preventDefault();
-
-      // see if we can find a chat object in the database
-      // to which we'll add the message
-      var chat = Chats.findOne({_id:Session.get("chatId")});
-      if (chat){// ok - we have a chat to use
-        var msgs = chat.messages; // pull the messages property
-        if (!msgs){// no messages yet, create a new array
-          msgs = [];
-        }
-        
-        // is a good idea to insert data straight from the form
-        // (i.e. the user) into the database?? certainly not. 
-        // push adds the message to the end of the array
-        msgs.push({text: event.target.chat.value,
-          senderId: Meteor.userId()});
-        
-        // reset the form
-        event.target.chat.value = "";
-        
-        // put the messages array onto the chat object
-        chat.messages = msgs;
-        
-        // update the chat object in the database.
-        Chats.update(chat._id, chat);
-      }
+      
+      var msg = event.target.chat.value;
+      var chatId = Session.get("chatId");
+       
+      Meteor.call("updateChat", chatId, msg);
     }
   })
 }
@@ -141,3 +124,35 @@ if (Meteor.isServer) {
     } 
   });
 }
+
+Meteor.methods({
+  createChat: function(user1Id, user2Id){
+    var chat = {
+      user1Id: user1Id,
+      user2Id: user2Id,
+      messages: []
+    }
+    
+    return Chats.insert(chat);
+  },
+  
+  updateChat: function(chatId, text){
+    var chat = Chats.findOne({_id: chatId});
+    var msgs = chat.messages;
+    
+    if (!msgs){
+      msgs = [];
+    }
+    
+    var msg = {
+      text: text,
+      senderId: this.userId
+    };
+    
+    msgs.push(msg);
+    chat.messages = msgs;
+    
+    Chats.update(chat._id,
+      chat);
+  }
+});
